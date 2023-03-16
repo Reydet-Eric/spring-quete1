252daa7e5cf77244d4b509bb9cf4b7b597789ae7
Reydet-Eric <eric.reydet@sncf.com>
2023-03-16 14:31:07 +0100

    first commit

---
+++ src/test/java/com/example/helloDoctor/HelloDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
